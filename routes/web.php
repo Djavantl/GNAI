bcb086c99b808083357bb4254d9ba0252cce9464
Djavantl <djavanlopesteixeira@gmail.com>
2026-01-05 17:24:47 -0300

    feat(person): criar CRUD de pessoa com model, controller, service e migration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonController;
+
+Route::get('/people', [PersonController::class, 'index'])->name('people.index');
+Route::get('/people/create', [PersonController::class, 'create'])->name('people.create');
+Route::post('/people/store', [PersonController::class, 'store'])->name('people.store');
+Route::get('/people/{person}/edit', [PersonController::class, 'edit'])->name('people.edit');
+Route::put('/people/{person}', [PersonController::class, 'update'])->name('people.update');
+Route::delete('/people/{person}', [PersonController::class, 'destroy'])->name('people.destroy');
