63e0f70a42f587dcf795411b753fb5d5820f6071
Djavantl <djavanlopesteixeira@gmail.com>
2026-07-27 12:07:52 -0300

    refactor(aee): adiciona Value Objects e exceptions para iniciar a migracao para o padrao DDD

---
+++ app/Domains/SpecializedEducationalSupport/Domain/Exceptions/SpecializedEducationalSupportException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\Domains\SpecializedEducationalSupport\Domain\Exceptions;
+
+use App\Exceptions\BusinessRuleException;
+
+abstract class SpecializedEducationalSupportException extends BusinessRuleException {}
