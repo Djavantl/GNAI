c1029923e166400ae6fd1e77a4690f5a58be9eaf
Djavantl <djavanlopesteixeira@gmail.com>
2026-08-06 01:13:59 -0300

    fix: ajusta vite config

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(({ command }) => ({
                 'resources/js/pages/dashboard.js',
                 'resources/js/components/photos.js',
                 'resources/js/pages/specialized-educational-support/attendance-record-form.js',
+                'resources/js/pages/specialized-educational-support/session.js',
                 'resources/js/pages/inclusive-radar/barriers.js',
                 'resources/js/pages/inclusive-radar/institutions.js',
                 'resources/js/pages/inclusive-radar/locations.js',
